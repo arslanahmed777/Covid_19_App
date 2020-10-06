dc92a82311b33d5ada3757d63c73f96c55d7a28e
Arslan Ahmed <67457531+arslanahmed777@users.noreply.github.com>
2020-10-06 09:59:38 +0500

    changes

---
+++ build/precache-manifest.fc5189a7b3f5aac83c2f8643347202a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a514b53bff9ce3682c805026532172f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6caa492161d2a3502737",
+    "url": "/static/css/main.757c8b43.chunk.css"
+  },
+  {
+    "revision": "2dc9480dfa748614c86b",
+    "url": "/static/js/2.c751a1e2.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.c751a1e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6caa492161d2a3502737",
+    "url": "/static/js/main.1e3488da.chunk.js"
+  },
+  {
+    "revision": "e6a718d9f09bd1673cbd",
+    "url": "/static/js/runtime-main.389ef5e8.js"
+  }
+]);
